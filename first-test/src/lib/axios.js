0aad34f0782b24544bb71d566b716702ac1f946d
Hasanor Dim <hasanordimasimpan@gmail.com>
2025-11-05 18:00:06 +0800

    To dev-branch4

--- first-test/src/lib/axios.js
+++ first-test/src/lib/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:5000"
-      : process.env.REACT_APP_API_URL,
+      ? "http://localhost:5000/api"
+      : `${process.env.REACT_APP_API_URL}/api`,
   withCredentials: true,
 });
 

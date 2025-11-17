b4ffe33edd4d69fa8647dcda19ae65e4df89280d
Hasanor Dim <hasanordimasimpan@gmail.com>
2025-11-17 15:52:52 +0800

    tested9

--- reimbursement-backend/src/config/db.js
+++ reimbursement-backend/src/config/db.js
@@ -1,15 +1,23 @@
-import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  logging: false,
-  define: { timestamps: true, underscored: true },
-  dialectOptions: {
-      ssl: process.env.DB_SSL === "true" ? { require: true, rejectUnauthorized: false } : false,
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: false,
+    define: { timestamps: true, underscored: true },
+    dialectOptions: {
+      ssl:
+        process.env.DB_SSL === "true"
+          ? { require: true, rejectUnauthorized: false }
+          : false,
     },
-});
+  }
+);
 
 export default sequelize;
